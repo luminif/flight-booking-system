146d02b398c279305b759dbcc6b257480cb5f8eb
luminif <arcocs72717@mail.ru>
2026-04-03 20:38:25 +0300

    Added custom annotations

---
+++ backend/src/main/java/ru/java/airport/annotations/IsEmployeeOrAdmin.java
@@ -0,0 +1,14 @@
+package ru.java.airport.annotations;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasAnyRole('EMPLOYEE', 'ADMIN')")
+public @interface IsEmployeeOrAdmin {
+}
---
+++ backend/src/main/java/ru/java/airport/annotations/IsUserOrAdmin.java
@@ -0,0 +1,18 @@
+package ru.java.airport.annotations;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("""
+	(#id == authentication.principal.id) or
+	(#userId == authentication.principal.id) or
+	hasRole('ADMIN')
+""")
+public @interface IsUserOrAdmin {
+}
