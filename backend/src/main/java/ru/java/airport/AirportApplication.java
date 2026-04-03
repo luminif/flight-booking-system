2e571899a58b221473c7173b57238302972f3281
luminif <arcocs72717@mail.ru>
2026-04-03 20:27:59 +0300

    Added annotation

--- backend/src/main/java/ru/java/airport/AirportApplication.java
+++ backend/src/main/java/ru/java/airport/AirportApplication.java
@@ -2,8 +2,11 @@ package ru.java.airport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import ru.java.airport.config.ApplicationConfig;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ApplicationConfig.class)
 public class AirportApplication {
 
 	public static void main(String[] args) {
